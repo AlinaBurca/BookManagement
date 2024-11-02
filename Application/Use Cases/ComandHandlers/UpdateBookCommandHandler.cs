2e72dea0f14bf65be7b36590877d3886ddc30dc3
AlinaBurca <burcaalina348@gmail.com>
2024-11-02 18:56:26 +0200

    Add project files.

---
+++ Application/Use Cases/ComandHandlers/UpdateBookCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using Application.Use_Cases.Commands;
+using Domain.Entities;
+using Domain.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Use_Cases.ComandHandlers
+{
+    public class UpdateBookCommandHandler : IRequestHandler<UpdateBookCommand, Guid>
+    {
+        private readonly IBookRepository repository;
+        public UpdateBookCommandHandler(IBookRepository repository) {
+            this.repository = repository;
+
+        }
+
+        public async Task<Guid> Handle(UpdateBookCommand request, CancellationToken cancellationToken)
+
+        {
+
+            var book = await repository.GetByIdAsync(request.Id);
+            if (book == null)
+            {
+                throw new KeyNotFoundException($"Book with Id {request.Id} not found.");
+            }
+
+            book.Title = request.Title;
+            book.Author = request.Author;
+            book.ISBN = request.ISBN;
+            book.PublicationDate = request.PublicationDate;
+
+            await repository.UpdateAsync(book);
+
+            return book.Id;
+
+        }
+    }
+}
